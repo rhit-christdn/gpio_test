c6c18e48de219521dd918c3775ea7138b7cc308a
Dylan Christopherson <dylan.christopherson@ottoexcellence.com>
2025-07-17 12:53:15 -0500

    changed to lgpio from pigpio

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include <pigpio.h>
+#include <lgpio.h>
 #include <iostream>
 #include <chrono>
 #include <thread>
@@ -6,36 +6,51 @@
 int main() {
     static const int PIN = 18; // BCM GPIO 17
 
-    std::cout << "pigpio version: " << gpioVersion() << std::endl;
-
-    if (gpioInitialise() < 0) {
-        std::cerr << "pigpio initialization failed." << std::endl;
+    int h = lgGpiochipOpen(0); // Open GPIO chip 0
+    if (h < 0) {
+        std::cerr << "Failed to open GPIO chip 0: " << lgError(h) << std::endl;
         return 1;
     }
-    std::cout << "pigpio initialized successfully." << std::endl;
 
-    // Set frequency if desired (default is ~800Hz) 
-    double dutyCycle = (50 / 100.0) * 1000000; // Convert percentage to 0-1000000 range
-    gpioHardwarePWM(PIN, 1000, dutyCycle);
+    while (true){
+        std::cout << "Enter a percent for duty cycle (0-100): ";
+        double dutycycle;
+        std::cin >> dutycycle;
 
-    double percent = 50.0;
+        lgTxPwm(h, PIN, 1000, dutyCycle, 0, 0);
 
-    while (true) {
-        std::cout << "Enter a percentage to set the duty cycle (0-100): ";
-        std::cin >> percent;
-        if (percent < 0 || percent > 100) {
-            std::cerr << "Invalid percentage. Please enter a value between 0 and 100." << std::endl;
-            continue;
-        }
-
-        dutyCycle = (percent / 100.0) * 1000000;
-        gpioHardwarePWM(PIN, 1000, dutyCycle); // Update PWM with new duty cycle/
-
-
-        std::this_thread::sleep_for(std::chrono::seconds(2));
     }
 
-    gpioTerminate();
+    // std::cout << "pigpio version: " << gpioVersion() << std::endl;
+
+    // if (gpioInitialise() < 0) {
+    //     std::cerr << "pigpio initialization failed." << std::endl;
+    //     return 1;
+    // }
+    // std::cout << "pigpio initialized successfully." << std::endl;
+
+    // // Set frequency if desired (default is ~800Hz) 
+    // double dutyCycle = (50 / 100.0) * 1000000; // Convert percentage to 0-1000000 range
+    // gpioHardwarePWM(PIN, 1000, dutyCycle);
+
+    // double percent = 50.0;
+
+    // while (true) {
+    //     std::cout << "Enter a percentage to set the duty cycle (0-100): ";
+    //     std::cin >> percent;
+    //     if (percent < 0 || percent > 100) {
+    //         std::cerr << "Invalid percentage. Please enter a value between 0 and 100." << std::endl;
+    //         continue;
+    //     }
+
+    //     dutyCycle = (percent / 100.0) * 1000000;
+    //     gpioHardwarePWM(PIN, 1000, dutyCycle); // Update PWM with new duty cycle/
+
+
+    //     std::this_thread::sleep_for(std::chrono::seconds(2));
+    // }
+
+    // gpioTerminate();
     return 0;
 }
 // Compile with: g++ -o main main.cpp -lpigpio -lrt -lpthread
