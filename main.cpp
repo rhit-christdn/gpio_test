e6d4d0ad00a16e36dde4657f69f6fa451232d063
Dylan Christopherson <dylan.christopherson@ottoexcellence.com>
2025-07-24 16:16:24 -0500

    push to talk wav file recording

--- main.cpp
+++ main.cpp
@@ -126,7 +126,7 @@ int main() {
         // }
 
         if (lgGpioRead(h, pin4)){
-            system("arecord recording.wav")
+            system("arecord recording.wav");
             std::this_thread::sleep_for(std::chrono(milliseconds(100)));
             while (lgGpioRead(h, pin4)){}
 
